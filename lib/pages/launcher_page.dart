2221056c452156596b9071c556960630c73dc3d7
NasirSunny50 <nasir.uddin08@northsouth.edu>
2022-02-15 00:08:00 +0600

    launcher_page updated

---
+++ lib/pages/launcher_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LauncherPage extends StatefulWidget {
+  static const String routeName = '/launcher';
+
+  @override
+  _LauncherPageState createState() => _LauncherPageState();
+}
+
+class _LauncherPageState extends State<LauncherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+
+      )
+    );
+  }
+}
